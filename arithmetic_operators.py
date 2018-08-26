ef045621764f870c7c97af3dbaa1a0f9870fa635
Jose Apablaza <japablaza@gmail.com>
2018-08-25 19:46:21 -0500

    adding the logic to the file

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+first_num = int(input())
+secon_num = int(input())
+
+two_num_sum  = first_num + secon_num
+two_num_diff = first_num - secon_num
+two_num_prod = first_num * secon_num
+
+print(two_num_sum)
+print(two_num_diff)
+print(two_num_prod)
